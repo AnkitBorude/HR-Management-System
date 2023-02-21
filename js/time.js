dd94b7f416ec76bac406caa2c9cc2c515cfbfff6
Ankit_Borude <ankitborude250@gmail.com>
2023-02-21 14:01:01 +0530

    Adding Clock and Date on navbar with Functionality

---
+++ js/time.js
@@ -0,0 +1,19 @@
+var currtime;
+var currdate;
+function viewdate() {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formattedToday = dd + '/' + mm + '/' + yyyy;
+    document.getElementById("date").innerHTML = currdate=formattedToday;
+}
+function updateClock()
+{
+    const today=new Date();
+    document.getElementById("time").innerHTML = currtime = today.toLocaleTimeString();
+}
